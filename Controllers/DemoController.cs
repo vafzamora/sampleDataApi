19dafe7c2a89db20ac902c3fa930d910f1ad17f4
vafzamora <vafzamora@gmail.com>
2020-06-26 08:44:23 -0300

    Samples 3 e 4

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -8,6 +8,8 @@ using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Net.Http;
+using System.Buffers;
+using System.IO;
 
 namespace sampleDataApi.Controllers
 {
@@ -18,72 +20,170 @@ namespace sampleDataApi.Controllers
 
         private readonly ILogger<DemoController> _logger;
         private readonly IConfiguration _configuration;
-        private readonly IHttpClientFactory _httpClientFactory;
-        private  Uri _imageUri; 
 
-        public DemoController(IConfiguration configuration, ILogger<DemoController> logger, IHttpClientFactory httpClientFactory)
+        private static string filePath = "./wwwroot/images/cover.jpg";
+        static ArrayPool<byte> arrayPool = ArrayPool<byte>.Create(200000, 50);
+
+        public DemoController(IConfiguration configuration, ILogger<DemoController> logger)
         {
             _logger = logger;
             _configuration = configuration;
-            _httpClientFactory = httpClientFactory;            
         }
 
         [HttpGet("sample1")]
-        public async Task<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
-             _imageUri =  new  Uri(new Uri($"{Request.Scheme}://{Request.Host}"),"/images/cover.jpg");
             string connectionString = _configuration.GetConnectionString("MusicStore");
             var result = new List<string>();
-            var httpClient = _httpClientFactory.CreateClient();
-            using(var conn = new SqlConnection(connectionString)){
-                var comm = new SqlCommand("SELECT TOP 20 ArtistId, Name FROM [dbo].[Artists]",conn);
-                var comm2 = new SqlCommand("SELECT AlbumId, Title, AlbumArtUrl FROM [dbo].[Albums] WHERE ArtistId = @ArtistId",conn);
-                comm2.Parameters.Add("@ArtistId",SqlDbType.Int);
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var comm = new SqlCommand("SELECT ArtistId, Name FROM [dbo].[Artists] WHERE ArtistId<=20", conn);
+                var comm2 = new SqlCommand("SELECT AlbumId, Title, AlbumArtUrl FROM [dbo].[Albums] WHERE ArtistId = @ArtistId", conn);
+                comm2.Parameters.Add("@ArtistId", SqlDbType.Int);
                 conn.Open();
-                using(var artistsReader = comm.ExecuteReader()){
-                while (artistsReader.Read())
+                using (var artistsReader = comm.ExecuteReader())
                 {
-                    comm2.Parameters["@ArtistId"].Value = artistsReader.GetInt32(0);
-                    using (var albumsReader = comm2.ExecuteReader()){
-                    while (albumsReader.Read())
+                    while (artistsReader.Read())
                     {
-                        
-                        var cover =  await httpClient.GetByteArrayAsync(_imageUri); 
+                        comm2.Parameters["@ArtistId"].Value = artistsReader.GetInt32(0);
+                        using (var albumsReader = comm2.ExecuteReader())
+                        {
+                            while (albumsReader.Read())
+                            {
 
-                        result.Add($"{artistsReader.GetString(1)}:{albumsReader.GetString(1)}");
-                    }}
+                                var cover = System.IO.File.ReadAllBytes(filePath);
 
-                }}
+                                result.Add($"{artistsReader.GetString(1)}:{albumsReader.GetString(1)}");
+                            }
+                        }
+
+                    }
+                }
             }
             return result;
         }
-    
+
 
         [HttpGet("sample2")]
         public async Task<IEnumerable<string>> GetV2()
         {
             string connectionString = _configuration.GetConnectionString("MusicStore");
             var result = new List<string>();
-            using(var conn = new SqlConnection(connectionString)){
-                var comm = new SqlCommand("SELECT TOP 20 ArtistId, Name FROM [dbo].[Artists]",conn);
-                var comm2 = new SqlCommand("SELECT AlbumId, Title, AlbumArtUrl FROM [dbo].[Albums] WHERE ArtistId = @ArtistId",conn);
-                comm2.Parameters.Add("@ArtistId",SqlDbType.Int);
-                await conn.OpenAsync();
-                using(var artistsReader = await comm.ExecuteReaderAsync()){
-                while (artistsReader.Read())
-                {
-                    comm2.Parameters["@ArtistId"].Value = artistsReader.GetInt32(0);
-                    using (var albumsReader = await comm2.ExecuteReaderAsync()){
-                    while (albumsReader.Read())
-                    {
-                        result.Add($"{artistsReader.GetString(1)}:{albumsReader.GetString(1)}");
-                    }}
 
-                }}
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var comm = new SqlCommand("SELECT ArtistId, Name FROM [dbo].[Artists] WHERE ArtistId<=20", conn);
+                var comm2 = new SqlCommand("SELECT AlbumId, Title, AlbumArtUrl FROM [dbo].[Albums] WHERE ArtistId = @ArtistId", conn);
+                comm2.Parameters.Add("@ArtistId", SqlDbType.Int);
+                await conn.OpenAsync();
+                using (var artistsReader = await comm.ExecuteReaderAsync())
+                {
+                    while (artistsReader.Read())
+                    {
+                        comm2.Parameters["@ArtistId"].Value = artistsReader.GetInt32(0);
+                        using (var albumsReader = await comm2.ExecuteReaderAsync())
+                        {
+                            while (albumsReader.Read())
+                            {
+                                var cover = await System.IO.File.ReadAllBytesAsync(filePath);
+
+                                result.Add($"{artistsReader.GetString(1)}:{albumsReader.GetString(1)}");
+                            }
+                        }
+
+                    }
+                }
             }
             return result;
         }
-    
 
+        [HttpGet("sample3")]
+        public async Task<IEnumerable<string>> GetV3()
+        {
+            string connectionString = _configuration.GetConnectionString("MusicStore");
+            var result = new List<string>();
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var comm = new SqlCommand("SELECT ArtistId, Name FROM [dbo].[Artists] WHERE ArtistId<=20", conn);
+                var comm2 = new SqlCommand("SELECT AlbumId, Title, AlbumArtUrl FROM [dbo].[Albums] WHERE ArtistId = @ArtistId", conn);
+                comm2.Parameters.Add("@ArtistId", SqlDbType.Int);
+                await conn.OpenAsync();
+                using (var artistsReader = await comm.ExecuteReaderAsync())
+                {
+                    while (artistsReader.Read())
+                    {
+                        comm2.Parameters["@ArtistId"].Value = artistsReader.GetInt32(0);
+                        using (var albumsReader = await comm2.ExecuteReaderAsync())
+                        {
+                            while (albumsReader.Read())
+                            {
+                                var cover = ArrayPool<byte>.Shared.Rent(196590);
+
+                                try
+                                {
+                                    using (var fileStream = System.IO.File.OpenRead(filePath))
+                                    {
+                                        using (var memStream = new MemoryStream(cover))
+                                        {
+                                            await fileStream.CopyToAsync(memStream);
+                                        }
+                                    }
+                                }
+                                finally
+                                {
+                                    ArrayPool<byte>.Shared.Return(cover, false);
+                                }
+
+                                result.Add($"{artistsReader.GetString(1)}:{albumsReader.GetString(1)}");
+                            }
+                        }
+
+                    }
+                }
+            }
+            return result;
+        }
+
+        [HttpGet("sample4")]
+        public async Task<IEnumerable<string>> GetV4()
+        {
+            string connectionString = _configuration.GetConnectionString("MusicStore");
+            var result = new List<string>();
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var comm = new SqlCommand(@"SELECT a.ArtistId, Name, AlbumId, Title
+                                            FROM [dbo].[Artists] a
+                                            INNER JOIN [dbo].[Albums] b ON a.ArtistId = b.ArtistId
+                                            WHERE a.ArtistId<=20", conn);
+                await conn.OpenAsync();
+                using (var artistsReader = await comm.ExecuteReaderAsync())
+                {
+                    while (artistsReader.Read())
+                    {
+                        var cover = ArrayPool<byte>.Shared.Rent(196590);
+                        try
+                        {
+                            using (var fileStream = System.IO.File.OpenRead(filePath))
+                            {
+                                using (var memStream = new MemoryStream(cover))
+                                {
+                                    await fileStream.CopyToAsync(memStream);
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            ArrayPool<byte>.Shared.Return(cover, false);
+                        }
+
+                        result.Add($"{artistsReader.GetString(1)}:{artistsReader.GetString(3)}");
+                    }
+                }
+
+            }
+            return result;
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,6 @@ namespace sampleDataApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
